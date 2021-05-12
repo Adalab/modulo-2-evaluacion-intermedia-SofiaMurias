17a779967d9dd0127b1a0cbc49ef233415a40683
SofiaMurias <sofiamuriaslopez@gmail.com>
2021-05-12 10:58:45 +0200

    add last change

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ const buttonElement = document.querySelector('.js-btn')
 const userNumber= document.querySelector('.js-inputNumber')
 const randomNumber = getRandomNumber()
 const boxtrack = document.querySelector('.js-track')
-const numberTry =document.querySelector('.js-try')
+const numberTry = document.querySelector('.js-try')
 
 
 function handlerclickbtn(){
-    getRandomNumber()
+    
     getNumberInput()
-    numberTry()
-
+    numberTrycount()
 }
 
 function getRandomNumber() {
@@ -19,12 +18,17 @@ function getRandomNumber() {
     return randomNumber;
 }  
 
-console.log('El número aleatorio es '+ getRandomNumber());
+console.log('El número aleatorio es '+ randomNumber);
 
 function getNumberInput(){
     const userTryNumber = parseInt(userNumber.value)
-    if (userTryNumber =  randomNumber){
+    console.log(userTryNumber);
+
+    if (userTryNumber ===  randomNumber){
         boxtrack.innerHTML = 'Has ganado campeona!!!';
+    }
+    else if (userTryNumber < 1 || userTryNumber > 100){
+        boxtrack.innerHTML = 'Prueba con un número del 1 al 100';
     }
     else if (userTryNumber < randomNumber){
         boxtrack.innerHTML = 'Demasiado bajo';
@@ -34,9 +38,6 @@ function getNumberInput(){
         boxtrack.innerHTML = 'Demasiado alto';
     }
     
-    else if (userTryNumber < 1 || userTryNumber > 100){
-        boxtrack.innerHTML = 'Prueba con un número del 1 al 100';
-    }
  console.log(boxtrack);
 }
 
@@ -45,7 +46,7 @@ function numberTrycount(){
     console.log(numberTry);
 }
 
-buttonElement.addEventListener = ('click', handlerclickbtn)
+buttonElement.addEventListener('click', handlerclickbtn);
 
 
 
