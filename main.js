a07f7dc9ac3d349b1b757ac31be0bb9a4a49832c
SofiaMurias <sofiamuriaslopez@gmail.com>
2021-05-25 20:30:04 +0200

    Clean code and console

--- main.js
+++ main.js
@@ -1,52 +1,38 @@
-'use strict'
+"use strict";
 
-const buttonElement = document.querySelector('.js-btn')
-const userNumber= document.querySelector('.js-inputNumber')
-const randomNumber = getRandomNumber()
-const boxtrack = document.querySelector('.js-track')
-const numberTry = document.querySelector('.js-try')
+const buttonElement = document.querySelector(".js-btn");
+const userNumber = document.querySelector(".js-inputNumber");
+const randomNumber = getRandomNumber();
+const boxtrack = document.querySelector(".js-track");
+const numberTry = document.querySelector(".js-try");
 
-
-function handlerclickbtn(){
-    getNumberInput()
-    numberTryCount()
+function handlerclickbtn() {
+  getNumberInput();
+  countTry();
 }
 
 function getRandomNumber() {
-   const randomNumber = Math.round(Math.random() * 100);
-    return randomNumber;
-}  
-
-console.log('El número aleatorio es '+ randomNumber);
-
-function getNumberInput(){
-    const userTryNumber = parseInt(userNumber.value)
-    console.log(userTryNumber);
-
-    if (userTryNumber ===  randomNumber){
-        boxtrack.innerHTML = 'Has ganado campeona!!!';
-    }
-    else if (userTryNumber < 1 || userTryNumber > 100){
-        boxtrack.innerHTML = 'Prueba con un número del 1 al 100';
-    }
-    else if (userTryNumber < randomNumber){
-        boxtrack.innerHTML = 'Demasiado bajo';
-    }
-         
-    else if (userTryNumber > randomNumber){
-        boxtrack.innerHTML = 'Demasiado alto';
-    }
-    
- console.log(boxtrack);
+  const randomNumber = Math.round(Math.random() * 100);
+  return randomNumber;
 }
 
-function numberTryCount(){
-    
-    numberTry.innerHTML= 'nº intentos'
-    console.log(numberTry);
+function getNumberInput() {
+  const userTryNumber = parseInt(userNumber.value);
+
+  if (userTryNumber === randomNumber) {
+    boxtrack.innerHTML = "Has ganado campeona!!!";
+  } else if (userTryNumber < 1 || userTryNumber > 100) {
+    boxtrack.innerHTML = "Prueba con un número del 1 al 100";
+  } else if (userTryNumber < randomNumber) {
+    boxtrack.innerHTML = "Demasiado bajo";
+  } else if (userTryNumber > randomNumber) {
+    boxtrack.innerHTML = "Demasiado alto";
+  }
 }
 
-buttonElement.addEventListener('click', handlerclickbtn);
+let counter = 0;
+function countTry() {
+  (counter += 1), (numberTry.innerHTML = "Número de intentos: " + counter);
+}
 
-
-
+buttonElement.addEventListener("click", handlerclickbtn);
